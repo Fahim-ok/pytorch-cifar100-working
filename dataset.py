3bb06f9f5c72f95b87d046b80239b0038cb63cc4
MD.Fahim-Ul-Islam <79527791+Fahim-ok@users.noreply.github.com>
2025-03-28 01:32:14 +0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -12,25 +12,28 @@ import numpy
 import torch
 from torch.utils.data import Dataset
 
+import torchvision
+from torch.utils.data import Dataset
+
 class CIFAR100Split(Dataset):
     """CIFAR-100 dataset split by classes for continual learning."""
 
-    def __init__(self, path, class_indices, transform=None):
+    def __init__(self, path, class_indices, transform=None, train=True):
         """
         Args:
             path (str): Path to the dataset.
             class_indices (list): List of class indices to include in this split.
             transform (callable, optional): Transform to apply to the images.
+            train (bool): Whether to load the training or test split.
         """
-        with open(os.path.join(path, 'train'), 'rb') as cifar100:
-            self.data = pickle.load(cifar100, encoding='bytes')
+        # Load the CIFAR-100 dataset using torchvision
+        self.dataset = torchvision.datasets.CIFAR100(root=path, train=train, download=True, transform=transform)
+        self.class_indices = set(class_indices)
 
-        self.transform = transform
-
-        # Filter data by class indices
+        # Filter the dataset by class indices
         self.filtered_indices = [
-            i for i, label in enumerate(self.data['fine_labels'.encode()])
-            if label in class_indices
+            i for i, (_, label) in enumerate(self.dataset)
+            if label in self.class_indices
         ]
 
     def __len__(self):
@@ -38,14 +41,7 @@ class CIFAR100Split(Dataset):
 
     def __getitem__(self, index):
         actual_index = self.filtered_indices[index]
-        label = self.data['fine_labels'.encode()][actual_index]
-        r = self.data['data'.encode()][actual_index, :1024].reshape(32, 32)
-        g = self.data['data'.encode()][actual_index, 1024:2048].reshape(32, 32)
-        b = self.data['data'.encode()][actual_index, 2048:].reshape(32, 32)
-        image = numpy.dstack((r, g, b))
-
-        if self.transform:
-            image = self.transform(image)
+        image, label = self.dataset[actual_index]
         return label, image
     
 class CIFAR100Test(Dataset):
