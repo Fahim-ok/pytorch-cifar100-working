c51b7772b414fd638189003101d59d852d27614c
MD.Fahim-Ul-Islam <79527791+Fahim-ok@users.noreply.github.com>
2025-03-28 01:25:22 +0600

    Update global_settings.py

--- conf/global_settings.py
+++ conf/global_settings.py
@@ -5,40 +5,26 @@ author baiyu
 import os
 from datetime import datetime
 
-#CIFAR100 dataset path (python version)
-#CIFAR100_PATH = '/nfs/private/cifar100/cifar-100-python'
+# Path to the CIFAR-100 dataset
+CIFAR100_PATH = './data/cifar100'  # Update this path as needed
 
-#mean and std of cifar100 dataset
+# Mean and std of CIFAR-100 dataset
 CIFAR100_TRAIN_MEAN = (0.5070751592371323, 0.48654887331495095, 0.4409178433670343)
 CIFAR100_TRAIN_STD = (0.2673342858792401, 0.2564384629170883, 0.27615047132568404)
 
-#CIFAR100_TEST_MEAN = (0.5088964127604166, 0.48739301317401956, 0.44194221124387256)
-#CIFAR100_TEST_STD = (0.2682515741720801, 0.2573637364478126, 0.2770957707973042)
-
-#directory to save weights file
+# Directory to save weights file
 CHECKPOINT_PATH = 'checkpoint'
 
-#total training epoches
-EPOCH = 2
+# Total training epochs
+EPOCH = 200
 MILESTONES = [60, 120, 160]
 
-#initial learning rate
-#INIT_LR = 0.1
-
+# Date format for logging
 DATE_FORMAT = '%A_%d_%B_%Y_%Hh_%Mm_%Ss'
-#time of we run the script
 TIME_NOW = datetime.now().strftime(DATE_FORMAT)
 
-#tensorboard log dir
+# TensorBoard log directory
 LOG_DIR = 'runs'
 
-#save weights file per SAVE_EPOCH epoch
+# Save weights file every SAVE_EPOCH epochs
 SAVE_EPOCH = 10
-
-
-
-
-
-
-
-
